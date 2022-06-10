199da9b326529f7bff6fc9ad75e96d8124612053
mhjoy99 <75517252+MHJoy99@users.noreply.github.com>
2022-06-11 00:11:47 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -65,20 +65,20 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Report Group", "https://t.me/+PRRzqHd31XY3ZWZk")
+    buttons.buildbutton("Main Group", "https://t.me/+Uh1txCXeyh80ODdl")
+    buttons.buildbutton("Request Group", "https://t.me/MotionPicturerequest2")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
-This bot can mirror all your links to Google Drive!
+This bot is for personal usage, only for MOTION PICTURE Admins!
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
         sendMarkup(start_string, context.bot, update.message, reply_markup)
     else:
-        sendMarkup('Not Authorized user, deploy your own mirror-leech bot', context.bot, update.message, reply_markup)
+        sendMarkup('U Cant Use, Ask MOTION PICTURE Admins For Help', context.bot, update.message, reply_markup)
 
 def restart(update, context):
-    restart_message = sendMessage("Restarting...", context.bot, update.message)
+    restart_message = sendMessage("Restart Kortesi...", context.bot, update.message)
     if Interval:
         Interval[0].cancel()
     clean_all()
